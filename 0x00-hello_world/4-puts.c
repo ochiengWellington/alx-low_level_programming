c1368e32f311b559eaedf6cc6aee957c6e6a0510
Ochieng Wellington <ochiengwellington22@gmail.com>
2023-03-16 11:54:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * 
- * Return: Always 0 
- */
+/* main - Entry point
+ * Return: Always 0
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
