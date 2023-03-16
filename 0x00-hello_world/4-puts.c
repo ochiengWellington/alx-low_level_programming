4bf6e995f4972b4272f6e6bf147f4f577f0122b0
Ochieng Wellington <ochiengwellington22@gmail.com>
2023-03-16 11:29:15 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main(void)
+/*This function prints to the standard output*/
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
