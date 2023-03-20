93836dc3a4dcef729867ac8ba9a04ae1964447db
Ochieng Wellington <ochiengwellington22@gmail.com>
2023-03-20 12:49:49 -0700

    lower then upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int l;
+        int a;
+
+        for (l = 'A'; l <= 'Z'; l++)
+        {
+                a = tolower(l);
+                putchar(a);
+		putchar(l);
+        }
+
+        putchar('\n');
+        return (0);
+}
